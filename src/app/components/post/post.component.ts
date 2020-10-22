4e8e1bdfc77aa57584b3fae8d57f42dd18583ae8
Fabian Chinchilla <fb.argenis@gmail.com>
2020-10-21 21:52:44 -0600

    -- DomSanitizer Pipe - Colocar imágenes en el background del slide

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -8,6 +8,9 @@ import { Component, Input, OnInit } from '@angular/core';
 export class PostComponent implements OnInit {
 
   @Input() post: Post = {};
+  img1 = "/assets/perro-1.jpg";
+  img2 = "/assets/perro-2.jpg";
+  img3 = "/assets/perro-3.jpg";
 
   constructor() { }
 

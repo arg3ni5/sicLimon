b40656b23a970f71306cdac1acb541bcb9c471a4
Fabian Chinchilla <fb.argenis@gmail.com>
2020-10-21 19:20:03 -0600

    -- Obtener el listado de nuestros Posts

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PostsService } from '../../services/posts.service';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,8 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private postService: PostsService) {
+    postService.getPosts().subscribe(posts => { console.log(posts) });
+  }
 
 }
---
+++ src/app/services/posts.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+const URL = environment.url;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsService {
+
+  pagePosts: number = 0;
+
+  constructor(private http: HttpClient) { }
+
+  getPosts() {
+    this.pagePosts++;
+    return this.http.get(`${URL}/posts/?page=${this.pagePosts}`)
+  }
+}
